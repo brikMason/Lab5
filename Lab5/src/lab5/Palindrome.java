31bb09015f5a3cb795ca5999c6c9597735d5bf64
wesmaxwell <wesmaxwell@10.30.15.162>
2017-09-28 10:21:00 -0500

    Initial commit - code added.

---
+++ Lab5/src/lab5/Palindrome.java
@@ -0,0 +1,46 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	
+	/**
+	 * This method tests if an input is a palindrome.
+	 * 
+	 * @author wesmaxwell
+	 * @param args
+	 * @since 2017-09-28
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a word or a number: ");
+		String pal = input.nextLine();
+		input.close();
+		if (isaPalindrome(pal))
+			System.out.println(pal + " is a palindrome.");
+		else
+			System.out.println(pal + " is not a palindrome.");
+
+	}
+
+	public static boolean isaPalindrome(String expr) {
+
+		// Adds ability to test names. Or any capital letter really...
+		expr = expr.toLowerCase();
+
+		// Considering any string length 0 or 1 to be a palindrome.
+		if (expr.length() == 0 || expr.length() == 1)
+			return true;
+		// Test first == last character, return substring if true
+		if (expr.charAt(0) == expr.charAt(expr.length() - 1)) {
+			return isaPalindrome(expr.substring(1, expr.length() - 1));
+		}
+		//
+		else {
+			return false;
+		}
+
+	}
+}
